84e781b58f9a6ef909e672f5472c420292ed579b
Birk Jefta Svensson <bis001@edu.zealand.dk>
2024-04-04 15:49:25 +0200

    04-04-2024

--- Pizzaria/MenuCatalog.cs
+++ Pizzaria/MenuCatalog.cs
@@ -8,10 +8,10 @@ using System.Threading.Tasks;
 namespace Pizzaria {
     public class MenuCatalog
     {
-        static List<Pizza> pizzas;
-        static MenuCatalog()
+        private static List<Pizza> _pizzas;
+        static  MenuCatalog()
         {
-            pizzas = new List<Pizza>();
+            _pizzas = new List<Pizza>();
         }
         public static void InitialPizzaMenu() {
             CreatePizza("Margherita", "Tomato & Cheese", 80);
@@ -22,7 +22,7 @@ namespace Pizzaria {
         {
             Pizza pizza = new Pizza(name,toppings,price);
             
-            pizzas.Add(pizza);
+            _pizzas.Add(pizza);
             
         }
 
@@ -30,7 +30,7 @@ namespace Pizzaria {
         {
             Console.WriteLine("Big Mamas Pizza Menu");
             int i = 1;
-            foreach (Pizza pizza in pizzas)
+            foreach (Pizza pizza in _pizzas)
             {
                 
                 
@@ -45,9 +45,9 @@ namespace Pizzaria {
             int pizzaByIndex;
             try
             {
-                pizzaByIndex = pizzas.IndexOf(pizzas[pizzanumber]);
-                Console.WriteLine($"you picked:{pizzas[pizzaByIndex]}");
-                return pizzas[pizzaByIndex];
+                pizzaByIndex = _pizzas.IndexOf(_pizzas[pizzanumber]);
+                Console.WriteLine($"you picked:{_pizzas[pizzaByIndex]}");
+                return _pizzas[pizzaByIndex];
             } 
             catch
             {
@@ -60,7 +60,7 @@ namespace Pizzaria {
         {
             string formattedName = char.ToUpper(name[0])+name.Substring(1);
             Pizza pizzaSearch = null;
-            foreach (Pizza pizza in pizzas)
+            foreach (Pizza pizza in _pizzas)
             {
                 if (pizza.Name == formattedName)
                 {
@@ -82,7 +82,7 @@ namespace Pizzaria {
         public static void DeletePizza(int number)
         {
             int deletedpizza = number - 1;
-            pizzas.Remove(pizzas[deletedpizza]);
+            _pizzas.Remove(_pizzas[deletedpizza]);
             
         }
     }
